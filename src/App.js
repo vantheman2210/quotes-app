bd243eb65f18d7f5baf991d93e7d7699fa99dcba
vantheman2210 <martinovic.ba@gmail.com>
2022-07-17 16:26:01 +0200

    formatting, and building routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <Routes>
+      <Route path="/AllQuotes"></Route> 
+      <Route path="/SingleQuote"></Route> 
+      <Route path="/NewQuote"></Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/SingleQuote.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SingleQuote = () => { 
+  return ( 
+    <div>
+      <h1>Single Quote</h1>
+    </div>
+  )
+}
